0f8a11f18d087a5d370e265ddc6b9e6894cbe00b
Lucas Bach <lborgesbach@gmail.com>
2023-12-10 15:03:30 -0300

    primeiro commit

---
+++ 01-aula/01aula.py
@@ -0,0 +1,6 @@
+nome = 'Lucas'
+idade = 32
+peso = 92
+nome = input('Qual seu nome?')
+print('Olá',nome,'!')
+print('É um prazer te conhecer!' )
---
+++ 02-primitivos_saida_de_dados/01aula/01int.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro:'))
+res = n1 + n2
+#print('A soma vale' , res)
+#print('A soma vale {}!' .format(res))
+print('A soma entre {} e {} vale {}' .format(n1, n2, res)) 
