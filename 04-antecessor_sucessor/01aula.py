9b44871a2919630aec71cbf5920652c9f9e1ef4f
Lucas Bach <lborgesbach@gmail.com>
2023-12-11 12:50:20 -0300

    ..

---
+++ 04-antecessor_sucessor/01aula.py
@@ -0,0 +1,13 @@
+n = int(input('Digite um número:'))
+
+print('Analisano o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, n - 1, n + 1))
+
+'''
+Poderia criar variáveis
+
+a = n - 1
+s = n + 1
+
+e adicionar as letras (variáveis), dentro de parênteses, onde está
+n - 1 e n + 1.
+'''
