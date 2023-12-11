be97d2ee5c461202abb621a246b80d4ad4858dea
Lucas Bach <lborgesbach@gmail.com>
2023-12-11 13:59:34 -0300

    adicionando aulas

---
+++ 09-conversor_de_moedas/01aula.py
@@ -0,0 +1,19 @@
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 4.94
+euro = real / 5.31
+print('Com R${:.2f} você pode comprar U${:.2f} e  €${:.2f} '.format(real, dolar, euro))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
